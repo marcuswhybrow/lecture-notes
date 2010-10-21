6b1e988b9fd1569654b7a608294d41cf82fe8a98
Marcus Whybrow <marcus@marcuswhybrow.net>
2010-10-21 07:45:10 +0100

    G53SRP 08 and modified conf.py to try and help the media out.

--- conf.py
+++ conf.py
@@ -66,7 +66,7 @@ release = '1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'media']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -122,7 +122,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'media']
+html_static_path = ['media']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
